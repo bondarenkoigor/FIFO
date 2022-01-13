4e1fd2b2bca02bfaa721d2b3b16f888d42551118
Игорь Бондаренко <Bond_fh54@student.itstep.org>
2022-01-13 17:17:49 +0200

    FIFO

---
+++ FIFO/FIFO/Source.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+
+template<typename T>
+struct Node
+{
+	T value;
+	Node<T>* next;
+
+	Node()
+	{
+		this->value = NULL;
+		this->next = nullptr;
+	}
+
+	Node(Node<T>* other)
+	{
+		this->value = other->value;
+		this->next = other->next;
+	}
+};
+
+template<typename T>
+class FIFO
+{
+private:
+	Node<T>* head;
+public:
+	FIFO()
+	{
+		head = nullptr;
+	}
+	void add(T value)
+	{
+		if (this->head == nullptr)
+		{
+			this->head = new Node<T>;
+			this->head->value = value;
+			this->head->next = nullptr;
+			return;
+		}
+		Node<T>* iter = this->head;
+		while (iter->next != nullptr)
+		{
+			iter = iter->next;
+		}
+		iter->next = new Node<T>;
+		iter->next->next = nullptr;
+		iter->next->value = value;
+	}
+	void print()
+	{
+		Node<T>* iter = this->head;
+		while (iter != nullptr)
+		{
+			std::cout << iter << "\t" << iter->next << "\t" << iter->value << "\n";
+			iter = iter->next;
+		}
+	}
+	Node<T>* remove()
+	{
+		Node<T>* forReturn = new Node<T>(this->head);
+		Node<T>* forDelete = this->head;
+		this->head = this->head->next;
+		delete forDelete;
+		return forReturn;
+	}
+};
+
+int main()
+{
+	FIFO<int> list;
+	list.add(1);
+	list.add(2);
+	list.add(3);
+	list.print();
+
+	std::cout << "\n" << list.remove()->value << "\n\n";
+	list.print();
+
+	std::cout << "\n" << list.remove()->value << "\n\n";
+	list.print();
+
+	std::cout << "\n" << list.remove()->value << "\n\n";
+	list.print();
+}
